b8a09eadc7a19def93c94143d759ae90288db2c1
Ananya Kulshrestha <105938861+Ananyakulshrestha123@users.noreply.github.com>
2023-06-28 08:28:56 +0530

    Update Python.py

--- Python.py
+++ Python.py
@@ -1,123 +1,78 @@
-import torch
-import torch.nn as nn
-import torch.optim as optim
-from torch.utils.data import Dataset, DataLoader
-from sklearn.preprocessing import StandardScaler, LabelEncoder
-from sklearn.neighbors import KernelDensity
-import pandas as pd
 import numpy as np
+from sklearn.neighbors import KernelDensity
 
-# Define the dataset class
-class TimeSeriesDataset(Dataset):
-    def __init__(self, data, targets):
-        self.data = data
-        self.targets = targets
-    
-    def __len__(self):
-        return len(self.data)
-    
-    def __getitem__(self, idx):
-        x = torch.tensor(self.data[idx], dtype=torch.float32)
-        y = torch.tensor(self.targets[idx])
-        return x, y
+def compute_covariance_matrix(class_samples, mean):
+    K = len(class_samples)  # Total number of classes
+    S = len(class_samples[0])  # Total number of samples in each class
+    D = len(class_samples[0][0])  # Dimension of the feature space
 
-# Define the Prototypical Network model
-class PrototypicalNetwork(nn.Module):
-    def __init__(self, input_dim, embedding_dim, hidden_dim):
-        super(PrototypicalNetwork, self).__init__()
-        self.fc1 = nn.Linear(input_dim, hidden_dim)
-        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
-        self.fc3 = nn.Linear(hidden_dim, hidden_dim)
-        self.fc4 = nn.Linear(hidden_dim, embedding_dim)
-        self.relu = nn.ReLU()
-        self.dropout = nn.Dropout(0.25)
-    
-    def forward(self, x, mask):
-        x = self.relu(self.fc1(x))
-        x = self.dropout(x)
-        x = self.relu(self.fc2(x))
-        x = self.dropout(x)
-        x = self.relu(self.fc3(x))
-        x = self.dropout(x)
-        x = self.fc4(x)
-        x = x * mask.unsqueeze(-1)  # Apply mask to zero out padded elements
-        return x
+    covariance_matrix = np.zeros((D, D))
 
-# Training Algorithm
-def train(model, dataloader, Nepisodes):
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
-    criterion = nn.CrossEntropyLoss()
+    for k in range(K):
+        for s in range(S):
+            diff = np.array(class_samples[k][s]) - mean
+            covariance_matrix += np.outer(diff, diff)
 
-    for episode in range(1, Nepisodes+1):
-        model.train()
-        optimizer.zero_grad()
+    covariance_matrix /= (K * S)
 
-        support_examples = []
-        for class_label in torch.unique(ytrain):
-            class_examples = Xtrain[ytrain == class_label]
-            support_indices = torch.randperm(class_examples.shape[0])[:S]
+    return covariance_matrix
 
-            # Create tensors of equal size for support examples
-            max_length = max([examples.shape[0] for examples in class_examples])
-            equal_size_examples = torch.zeros((S, max_length, class_examples.shape[1]), dtype=torch.float32)
+def algorithm(Xtrain, ytrain, Xcal, ycal, Scal, fθ):
+    K = len(set(ytrain))  # Total number of classes
+    S = Scal  # Number of support examples for each class
 
-            # Fill tensors with support examples
-            for i, index in enumerate(support_indices):
-                equal_size_examples[i, :class_examples[index].shape[0], :] = torch.tensor(class_examples[index], dtype=torch.float32)
+    class_samples = [[] for _ in range(K)]  # List to store support examples for each class
 
-            support_examples.append(equal_size_examples)
+    # Collect support examples for each class
+    for i, label in enumerate(ytrain):
+        class_samples[label].append(fθ(Xtrain[i]))
 
-        prototypes = torch.mean(model(torch.cat(support_examples, dim=0)), dim=1)
+    # Compute µk for each class k
+    means = []
+    for class_samples_k in class_samples:
+        mean_k = np.mean(class_samples_k, axis=0)
+        means.append(mean_k)
 
-        for x, y in dataloader:
-            x = x.to(device)
-            y = y.to(device)
+    # Compute µ0
+    mean_0 = np.mean(class_samples, axis=(0, 1))
 
-            embeddings = model(x)
-            logits = -torch.cdist(embeddings, prototypes, p=2)
-            class_probs = nn.functional.softmax(logits, dim=1)
+    # Compute Σ
+    covariance_matrix = compute_covariance_matrix(class_samples, means)
 
-            loss = criterion(logits, y)
-            loss.backward()
-            optimizer.step()
+    # Compute Σ0
+    covariance_matrix_0 = compute_covariance_matrix(class_samples, mean_0)
 
-    return model
+    # Compute Σk for each class k
+    covariance_matrices_k = []
+    for class_samples_k in class_samples:
+        covariance_matrix_k = compute_covariance_matrix(class_samples_k, means)
+        covariance_matrices_k.append(covariance_matrix_k)
 
+    # Compute relative Mahalanobis distances for calibration examples
+    Mahalanobis_distances = []
+    for i in range(len(Xcal)):
+        Mahalanobis_distance_i = np.zeros(K)
+        for k in range(K):
+            diff = fθ(Xcal[i]) - means[k]
+            Mahalanobis_distance_i[k] = np.sqrt(np.dot(np.dot(diff, np.linalg.inv(covariance_matrices_k[k])), diff))
+        Mahalanobis_distances.append(Mahalanobis_distance_i)
 
-# Load the dataset
-data = pd.read_csv(r'E:\http_new_comx_analysis.csv')
-df = data.drop(['Flow'], axis=1)
+    # Fit Gaussian kernel density estimate to the set of relative Mahalanobis distances
+    Gaussian_kernels = []
+    for k in range(K):
+        kernel = KernelDensity(bandwidth=0.2, kernel='gaussian')
+        kernel.fit(Mahalanobis_distances[k].reshape(-1, 1))
+        Gaussian_kernels.append(kernel)
 
-# Apply LabelEncoder to encode categorical labels
-label_encoder = LabelEncoder()
-df_encoded = df.apply(label_encoder
-                      
-                      
- 
-                      
-                      
-  equal_size_examples[i, :class_examples[index].shape[0], :] = torch.tensor(class_examples[index], dtype=torch.float32)
-                                       
-           equal_size_examples[i, :class_examples[index].shape[0], :] = torch.tensor(class_examples[index], dtype=torch.float32)
+    # Return necessary results
+    return means, mean_0, covariance_matrix, covariance_matrix_0, Gaussian_kernels
 
-                      
-def forward(self, x, mask=None):
-    x = self.relu(self.fc1(x))
-    x = self.dropout(x)
-    x = self.relu(self.fc2(x))
-    x = self.dropout(x)
-    x = self.relu(self.fc3(x))
-    x = self.dropout(x)
-    x = self.fc4(x)
-    if mask is not None:
-        x = x * mask.unsqueeze(-1)  # Apply mask to zero out padded elements
-    return x
- 
-   for x, y in dataloader:
-    embeddings = model(x, mask=(x != 0).float())  # Pass mask argument to model
-    ...
-                   
-                      
-                      
-                      
-                      
+# Example usage
+Xtrain = [[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]]  # Training feature vectors
+ytrain = [0, 1, 0, 2, 1]  # Training labels
+Xcal = [[2, 3], [4, 5], [6, 7]]  # Calibration feature vectors
+ycal = [0, 1, 2]  # Calibration labels
+Scal = 2  # Number of support examples for each class
+
+def fθ(x):
+    return np.array(x)  # Placeholder function for embedding calculation
